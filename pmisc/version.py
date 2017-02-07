db9044d16b8c3cd6c45fc685243343bdc6f35d07
Pablo Acosta-Serafini <pmasdev@gmail.com>
2017-02-07 08:31:10 -0500

    More continuous integration fixes

--- pmisc/version.py
+++ pmisc/version.py
@@ -7,7 +7,7 @@
 ###
 # Global variables
 ###
-VERSION_INFO = (1, 2, 1, 'candidate', 4)
+VERSION_INFO = (1, 2, 1, 'candidate', 5)
 
 
 ###
--- sbin/build_docs.py
+++ sbin/build_docs.py
@@ -2,7 +2,7 @@
 # build_docs.py
 # Copyright (c) 2013-2017 Pablo Acosta-Serafini
 # See LICENSE for details
-# pylint: disable=C0111,C0411,C0413,F0401,R0912,R0914,R0915,W0141
+# pylint: disable=C0111,C0411,C0413,E0611,F0401,R0912,R0914,R0915,W0141
 
 # Standard library imports
 from __future__ import print_function
