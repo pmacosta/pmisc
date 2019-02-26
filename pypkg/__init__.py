bb43f48d3c701319ed4cd1fdbd7a694ca9c9adf0
Pablo Acosta-Serafini <pmasdev@gmail.com>
2019-02-26 12:00:04 -0500

    Moved package management framework to its own repository

--- pypkg/__init__.py
+++
@@ -1,6 +0,0 @@
-# __init__.py
-# Copyright (c) 2013-2019 Pablo Acosta-Serafini
-# See LICENSE for details
-# pylint: disable=C0111
-
-pass
--- pypkg/list-authors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# list-authors.sh
-# Copyright (c) 2013-2019 Pablo Acosta-Serafini
-# See LICENSE for details
-
-echo "Project committers:"
-echo "-------------------"
-git log --format='%aN <%aE>' | sort -u
